03df1070efa7f85000588c139e277a7ae583db56
pedrocorrea2002 <pedro.hcorrea15@gmail.com>
2023-11-27 21:00:21 -0300

    first commit

---
+++ android/app/src/main/kotlin/com/example/av2_folha_pagamento/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.av2_folha_pagamento
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/pages/home.dart
@@ -0,0 +1,111 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final TextEditingController matricula = TextEditingController();
+    final TextEditingController nome = TextEditingController();
+
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            "Folha de pagamento",
+            style: TextStyle(color: Colors.black),
+          ),
+          backgroundColor: Colors.white,
+        ),
+        body: Container(
+          padding: const EdgeInsets.only(top: 40, left: 30, right: 30),
+          child: ListView(
+            children: [
+              Row(
+                children: [
+                  const Expanded(
+                      flex: 1,
+                      child: Text(
+                        "Matrícula:",
+                        style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold),
+                      )),
+                  Expanded(
+                      flex: 3,
+                      child: TextFormField(
+                          controller: matricula,
+                          keyboardType: TextInputType.number,
+                          decoration: const InputDecoration(
+                            contentPadding: EdgeInsets.all(10),
+                            border: OutlineInputBorder(
+                                borderSide: BorderSide(
+                                    color: Colors.black12, width: 10)),
+                          )))
+                ],
+              ),
+              const Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  SizedBox(height: 30),
+                  Text(
+                    "Funcionário:",
+                    style: TextStyle(
+                        color: Colors.black,
+                        fontSize: 20,
+                        fontWeight: FontWeight.bold),
+                  ),
+                  SizedBox(
+                      height: 300,
+                      width: 200,
+                      child: Image(
+                          image: NetworkImage(
+                              'https://i.pinimg.com/736x/a3/71/4b/a3714ba657487833c35ef16632f7b896.jpg'))),
+                  SizedBox(
+                      height: 300,
+                      width: 200,
+                      child: Image(
+                          image: NetworkImage(
+                              'https://www.diariodocentrodomundo.com.br/wp-content/uploads/2014/07/mussum-1.jpg'))),
+                  SizedBox(
+                      height: 200,
+                      width: 200,
+                      child: Image(
+                          fit: BoxFit.cover,
+                          image: NetworkImage(
+                              'https://s2-g1.glbimg.com/vbxlmE70yvlrZOlq0Jd0Q5FzXwQ=/0x0:730x489/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2022/E/6/6a7nQrT1uf8IrQjSYflQ/csm-pincel-413e3aba36.jpg')))
+                ],
+              )
+            ],
+          ),
+        ),
+        persistentFooterButtons: [
+          ButtonBar(
+            alignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              ElevatedButton(
+                  onPressed: () {
+                    // double av1Value = double.parse(av1.text);
+                    // double av2Value = double.parse(av2.text);
+
+                    // media.text = "${(av1Value + av2Value) / 2}";
+                  },
+                  child: const Text("Calcular")),
+              ElevatedButton(
+                onPressed: () {
+                  matricula.clear();
+                  nome.clear();
+                },
+                child: const Text("Limpar"),
+              ),
+              ElevatedButton(
+                  onPressed: () {
+                    SystemNavigator.pop();
+                  },
+                  child: const Text("Sair"))
+            ],
+          )
+        ]);
+  }
+}
